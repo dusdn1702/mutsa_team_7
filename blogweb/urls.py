98b8f75f4fe4bc64130a4acb659946788daf68e2
callaway12 <taewankim0925@likelion.org>
2019-04-02 19:51:06 +0900

    all done

--- blogweb/urls.py
+++ blogweb/urls.py
@@ -6,4 +6,8 @@ import blogweb.views
 
 urlpatterns =[
     path('', blogweb.views.home, name='home'),
+    path('post/', blogweb.views.post, name='post'),
+    path('post/<int:post_id>/', blogweb.views.detail, name='detail'),
+    path('post/new/', blogweb.views.new, name='new'),
+    path('post/create/', blogweb.views.create, name='create'),
 ]
